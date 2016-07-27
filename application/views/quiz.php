db41035deb4b7975547ba1cb7587570a8105e2ef
asimbilaladil <asimbilal@Asims-MacBook.local>
2016-07-28 00:05:20 +0500

    add quiz page

---
+++ application/views/quiz.php
@@ -0,0 +1,52 @@
+<div class="featured-content">
+    <div class="substrate">
+        <img src="http://www.tdcsinstitute.com/wp-content/themes/academy/images/bgs/site_bg.jpg" class="fullwidth" alt="">			
+    </div>
+    <div class="row">
+        <div class="page-title">
+            <h1 class="nomargin">  Quiz</h1>
+        </div>
+        <!-- /page title -->				
+    </div>
+</div>
+<div class="main-content">
+			<div class="row"><div class="eightcol column">
+	<h1>Sample Lesson Quiz</h1>
+	<p>Donec feugiat tincidunt lectus a interdum. Nunc vulputate, nunc ut elementum iaculis, libero mi rhoncus leo, fringilla convallis mi ligula in erat. Integer accumsan nisl ac eros consectetur nec dictum orci tempus. Proin mollis gravida accumsan. In cursus iaculis sapien eget sollicitudin. Sed at ante dolor. Pellentesque hendrerit, dolor sit amet laoreet aliquam, purus neque dignissim turpis, nec imperdiet felis nisl nec urna.&nbsp;Nulla lacinia facilisis nunc quis gravida.</p>
+	<div class="quiz-listing">
+		<form id="quiz_form" action="http://www.tdcsinstitute.com/quiz/sample-lesson-quiz/" method="POST">
+			<div class="message">
+							</div>
+						<div class="quiz-question multiple">
+				<div class="question-title">
+					<div class="question-number">1</div>
+					<h4 class="nomargin">Donec feugiat adipiscing convallis dolor dictum?</h4>
+
+				</div>
+
+					<h6 class="nomargin" style="color:red">Error Message wrong answer</h6>
+				<ul><li class=""><input type="checkbox" name="q52589194b85b184[a52589194b85f150]" id="q52589194b85b184_a52589194b85f150" value="true"><label for="q52589194b85b184_a52589194b85f150">Dolor</label></li><li class=""><input type="checkbox" name="q52589194b85b184[a52589194b862e59]" id="q52589194b85b184_a52589194b862e59" value="true"><label for="q52589194b85b184_a52589194b862e59">Dictum</label></li><li class=""><input type="checkbox" name="q52589194b85b184[a52589194b866863]" id="q52589194b85b184_a52589194b866863" value="true"><label for="q52589194b85b184_a52589194b866863">Libero</label></li><li class=""><input type="checkbox" name="q52589194b85b184[a52589194b86a241]" id="q52589194b85b184_a52589194b86a241" value="true"><label for="q52589194b85b184_a52589194b86a241">Lorem</label></li></ul>			</div>
+						<div class="quiz-question multiple">
+				<div class="question-title">
+					<div class="question-number">34</div>
+					<h4 class="nomargin">Quisque quam lorem ipsum fermentum libero?</h4>
+				</div>
+				<ul><li class=""><input type="checkbox" name="q52589194b86dc53[a52589194b871537]" id="q52589194b86dc53_a52589194b871537" value="true"><label for="q52589194b86dc53_a52589194b871537">Ultricies</label></li><li class=""><input type="checkbox" name="q52589194b86dc53[a52589194b875360]" id="q52589194b86dc53_a52589194b875360" value="true"><label for="q52589194b86dc53_a52589194b875360">Turpis</label></li><li class=""><input type="checkbox" name="q52589194b86dc53[a52589194b878c86]" id="q52589194b86dc53_a52589194b878c86" value="true"><label for="q52589194b86dc53_a52589194b878c86">Donec</label></li><li class=""><input type="checkbox" name="q52589194b86dc53[a52589194b87c729]" id="q52589194b86dc53_a52589194b87c729" value="true"><label for="q52589194b86dc53_a52589194b87c729">Augue</label></li></ul>			</div>
+						<div class="quiz-question multiple">
+				<div class="question-title">
+					<div class="question-number">3</div>
+					<h4 class="nomargin">Curabitur lectus lorem ultricies ultrices amet?</h4>
+				</div>
+				<ul><li class=""><input type="checkbox" name="q52589194b880142[a52589194b883b96]" id="q52589194b880142_a52589194b883b96" value="true"><label for="q52589194b880142_a52589194b883b96">Neque</label></li><li class=""><input type="checkbox" name="q52589194b880142[a52589194b887411]" id="q52589194b880142_a52589194b887411" value="true"><label for="q52589194b880142_a52589194b887411">Amet</label></li><li class=""><input type="checkbox" name="q52589194b880142[a52589194b88ae52]" id="q52589194b880142_a52589194b88ae52" value="true"><label for="q52589194b880142_a52589194b88ae52">Luctus</label></li><li class=""><input type="checkbox" name="q52589194b880142[a52589194b88e775]" id="q52589194b880142_a52589194b88e775" value="true"><label for="q52589194b880142_a52589194b88e775">Semper</label></li></ul>			</div>
+						<input type="hidden" name="course_action" value="complete_course">
+			<input type="hidden" name="lesson_action" value="complete_quiz">
+			<input type="hidden" name="course_id" value="0">
+			<input type="hidden" name="lesson_id" value="454">
+			<input type="hidden" name="nonce" class="nonce" value="50b0ad6181">
+			<input type="hidden" name="action" class="action" value="themex_update_lesson">
+		</form>
+	</div>
+</div>
+
+								</div>
+			</div>
