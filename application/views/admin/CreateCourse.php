b13dfe0e57cac964ac5e809ad7d4125e7ea94f96
asimbilaladil <asimbilal@Asims-MacBook.local>
2016-07-30 19:56:15 +0500

    fix create course page css

--- application/views/admin/CreateCourse.php
+++ application/views/admin/CreateCourse.php
@@ -21,32 +21,26 @@
                     <div class="col-md-12">
                         <div class="col-md-12">
                             <form class="ajax-form formatted-form" action="<?php echo site_url('CreateCourse/save') ?>" method="POST">
-
                                 <div class="box box-success">
                                     <div class="box-header with-border">
                                         <h3 class="box-title">Create New Course</h3>
                                     </div>
-                                    
                                     <div class="box-body">
-                                      <div class="form-group">
+                                        <div class="form-group">
                                             <label for="" class="col-sm-2 control-label">Name</label>
                                             <div class="col-sm-6">
                                                 <input type="text" name="name" class="form-control" id="" placeholder="Course Name" required>
                                             </div>
                                             <br><br>
                                         </div>
-
-
-                                      <div class="form-group">
+                                        <div class="form-group">
                                             <label for="" class="col-sm-2 control-label">Presentation URL</label>
                                             <div class="col-sm-6">
                                                 <input type="text" name="url" class="form-control" id="" placeholder="URL" required>
                                             </div>
                                             <br><br>
                                         </div>
-
-
-                                      <div class="form-group">
+                                        <div class="form-group">
                                             <label for="" class="col-sm-2 control-label">Description</label>
                                             <div class="col-sm-6">
                                                 <input type="text" name="description" class="form-control" id="" placeholder="Description" required>
@@ -55,80 +49,39 @@
                                         </div>
                                         <div class="form-group">
                                             <div class="col-sm-offset-2 col-sm-2">
-
                                                 <button type="submit" class="btn btn-primary btn-block">Save</button>
                                             </div>
-                                        </div>                                                                               
-          
-
+                                        </div>
+                                        <br><br>    
+                                        <h3><span>&nbsp;&nbsp;</span> Modules: </h3>
+                                        <table style="width:100%" class="table-striped" >
+                                            <thead>
+                                                <tr>
+                                                    <th style="padding: 15px;" >Name</th>
+                                                    <th style="padding: 15px;">Action</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <?php
+                                                    foreach ($data['courses'] as $course) {
+                                                             
+                                                               echo '<tr>
+                                                                 <td style="padding: 15px;"> '. $course->name .' </td>
+                                                                 <td style="padding: 15px;"> <a href=""> VIEW </a></td>
+                                                               </tr>';
+                                                             
+                                                             }
+                                                    ?>                  
+                                            </tbody>
+                                        </table>
                                     </div>
-                           
-
                             </form>
+                            </div>
                         </div>
-
-                        </div>
-       
-
                     </div>
                 </div>
                 <!-- /.row (main row) -->
             </section>
             <!-- /.content -->
-            <!-- Main content -->
-            <section class="content">
-                <!-- Main row -->
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="col-md-12">
-
-                                <div class="box box-success">
-                                    <div class="box-header with-border">
-                                        <h3 class="box-title">Modules</h3>
-                                    </div>
-                                    
-                                    <div class="box-body">
-                                 <table style="width:100%" class="table-striped" >
-              <thead>
-                <tr>
-                  <th style="padding: 15px;" >Name</th>
-                  <th style="padding: 15px;">Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                <?php
-                         foreach ($data['courses'] as $course) {
-                                  
-                                    echo '<tr>
-                                      <td style="padding: 15px;"> '. $course->name .' </td>
-                                      <td style="padding: 15px;"> <a href=""> VIEW </a></td>
-                                    </tr>';
-                                  
-                                  }
-                  ?>                  
-              </tbody>
-            </table>
-
-
-
-
-                                
-                                                                                                                    
-          
-
-                                    </div>
-                           
-
-
-                        </div>
-
-                        </div>
-       
-
-                    </div>
-                </div>
-                <!-- /.row (main row) -->
-            </section>
-            <!-- /.content -->            
         </div>
     </div>
