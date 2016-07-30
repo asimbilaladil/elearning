65df74f816069d8f5f2e96231d4697237a0f1905
asimbilaladil <asimbilal@Asims-MacBook.local>
2016-07-30 11:55:28 +0500

    add view module page

---
+++ application/views/ViewModule.php
@@ -0,0 +1,64 @@
+<div class="featured-content">
+    <div class="substrate">
+        <img src="http://www.tdcsinstitute.com/wp-content/themes/academy/images/bgs/site_bg.jpg" class="fullwidth" alt="">          
+    </div>
+    <div class="row">
+        <div class="page-title">
+            <h1 class="nomargin">  Name of module</h1>
+        </div>
+        <!-- /page title -->                
+    </div>
+</div>
+<div class="main-content">
+    <div class="row">
+        <div class="column eightcol">
+            <article class="single-post">
+                <div class="post-content">
+                    <h1>Module I</h1>
+                    <div class="learndash user_has_access" id="learndash_post_2443">
+                        <p><iframe src="https://docs.google.com/presentation/d/1E-LbWvgvCA952kLPYkGGFoSv3PLLVhPp7lRg7_fBAy4/embed?start=true&amp;loop=false&amp;delayms=3000" width="700" height="554" frameborder="0" allowfullscreen="allowfullscreen"></iframe></p>
+            
+                        <br>
+
+                    </div>
+      
+                </div>
+            </article>
+       
+        </div>
+        <aside class="sidebar column fourcol last">
+            <div class="widget widget_recent_entries">
+                <div class="widget-title">
+                    <h3 class="nomargin">Quiz</h3>
+                </div>
+                <ul>
+                    <li>
+                        <a href="http://www.tdcsinstitute.com/demo/">Start Quiz</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="widget widget_recent_entries">
+                <div class="widget-title">
+                    <h3 class="nomargin">List Of Modules</h3>
+                </div>
+                <ul>
+                    <li>
+                        <a href="http://www.tdcsinstitute.com/demo/">Demo</a>
+                    </li>
+                    <li>
+                        <a href="http://www.tdcsinstitute.com/learn-about-cpe-consulting/">Learn About CPE Consulting</a>
+                    </li>
+                    <li>
+                        <a href="http://www.tdcsinstitute.com/tdcs-occupational-task-enhancement/">tDCS Occupational Task Enhancement</a>
+                    </li>
+                    <li>
+                        <a href="http://www.tdcsinstitute.com/scientific-and-ethical-issues-with-tdcs/">Scientific and Ethical Issues with tDCS</a>
+                    </li>
+                </ul>
+            </div>
+
+
+
+        </aside>
+    </div>
+</div>
