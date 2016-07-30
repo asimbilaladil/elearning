9fdb5a5797e97214c68e3b0369cb5efc9b24f729
asimbilaladil <asimbilal@Asims-MacBook.local>
2016-07-30 19:12:37 +0500

    fix what we offer page

--- application/views/what-we-offer.php
+++ application/views/what-we-offer.php
@@ -1,17 +1,29 @@
 <div class="featured-content">
     <div class="substrate">
-        <img src="http://www.tdcsinstitute.com/wp-content/themes/academy/images/bgs/site_bg.jpg" class="fullwidth" alt="">			
+        <img src="http://www.tdcsinstitute.com/wp-content/themes/academy/images/bgs/site_bg.jpg" class="fullwidth" alt="">          
     </div>
     <div class="row">
         <div class="page-title">
             <h1 class="nomargin">  What-we-offer</h1>
         </div>
-        <!-- /page title -->				
+        <!-- /page title -->                
     </div>
 </div>
 <div class="main-content">
     <div class="row">
-        <h3 style="text-align: justify;">Welcome to the tDCS Institute’s free online course offering you the fundamentals behind the cognitive performance enhancement by means of the latest neuroscience adopted techniques and applications. The course is specifically developed to provide a student with just enough information about the use of tDCS technology, as is minimally required to qualify as a consultant within the cognitive performance enhancement professional field. A quick registration on your part is necessary to allow us to offer more of the Institute’s resources throughout the learning process. Please take a few minutes of your time to register for the free course that may just put you on the path to a new and exciting professional career.</h3>
+    <article class="post-400 post type-post status-publish format-standard hentry category-news tag-amet tag-ipsum tag-lorem post clearfix instock">
+        <div class="column thirdcol post-image">
+        <div class=" thick-border">
+            <img style="width: 200px;height: 200px;"  alt="image_57" class="attachment-normal wp-post-image" src="<?php echo base_url("assets/images/services.png") ?>">
+        </div>
+    </div>
+    <div class="post-content column ninecol last">
+
+        <p style="text-align: justify;" >Welcome to the tDCS Institute’s free online course offering you the fundamentals behind the cognitive performance enhancement by means of the latest neuroscience adopted techniques and applications. The course is specifically developed to provide a student with just enough information about the use of tDCS technology, as is minimally required to qualify as a consultant within the cognitive performance enhancement professional field. A quick registration on your part is necessary to allow us to offer more of the Institute’s resources throughout the learning process. Please take a few minutes of your time to register for the free course that may just put you on the path to a new and exciting professional career.</p>
         <a href="<?php echo site_url('Register') ?>" target="_self" class=" button large primary"><img class="alignnone" alt="" src="<?php echo base_url("assets/images/image_4.png") ?>">Continue&nbsp;</a>
+
+    </div>
+    </article>
+
     </div>
 </div>
