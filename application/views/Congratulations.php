e369b72166b34a21eae1f541723fba0e13ed8e73
asimbilaladil <asimbilal@Asims-MacBook.local>
2016-08-03 00:14:19 +0500

    add paypal page text

--- application/views/Congratulations.php
+++ application/views/Congratulations.php
@@ -11,7 +11,14 @@
 </div>
 <div class="main-content">
     <div class="row">
-        <div class="column eightcol" style="text-align: center;">
+        <div class="column twelvecol" style="text-align: justify; font-size: 17px;">
+            <p> Congratulations!  You have now completed all tutorial-modules required for the Institute’s certification of your professional credentials as a CPE Consultant. </p> 
+            <p>  Consultants are professionals who provide expert advice in a specific area of knowledge.  Our consultants are not therapists or providers of any type of medical service to their clients, rather they are contracted to help find solutions to client’s performance enhancement requirements. </p>
+            <p> Those independently self-employed essentially operate their own business.  In the area of cognitive performance enhancement, CPE consultants are commonly contracted as productivity experts.  Managers of businesses, in which employees deal with high-concentration requirements, find the most value in hiring them. </p>
+            <p> Over the past few years, we have witnessed a growing consumer demand for consultants who are trained and certified in the use of this technology.  The demand for such professional services exceeds the supply of certified specialists entering the marketplace. </p>
+            <p> Now, it is your opportunity to get certified as a CPE Consultant and begin new and rewarding career in cognitive performance enhancement industry.  Please take a moment to get the Institute’s certification of your professional credentials. </p>
+
+            </p>
             <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
 
                 <!-- Identify your business so that you can collect the payments. -->
