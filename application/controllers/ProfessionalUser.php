18812062bd72552777ce2d22f0e18d85ca32d291
mmoiz129 <muhammadmoiz129@gmail.com>
2016-08-14 18:29:06 +0500

    done

--- application/controllers/ProfessionalUser.php
+++ application/controllers/ProfessionalUser.php
@@ -28,21 +28,53 @@ class ProfessionalUser extends CI_Controller {
         $userId = $this->session->userdata('id');
         $certificateData = $this->CertificationModel->getUserById($userId);
 
-        $data = array(
-            'fullName' => $this->input->post('fullName', true),
-            'certificateNumber  ' => $this->input->post('certificateNumber', true),
-            'email' => $this->input->post('email', true),
-            'businessAddress' => $this->input->post('businessAddress', true),
-            'telephoneNumber' => $this->input->post('telNumber', true),
-            'userId' => $userId,
-            'password' => $this->input->post('password', true),
-            'websiteUrl' => $this->input->post('website', true),
-            'facebook' => $this->input->post('facebook', true),
-            'country' => $certificateData->country,
-            'city' => $certificateData->city
-            );
+        $uploadPath = './uploads/ProfilePictures/';                //make upload path 
 
-        $this->User->professionalUserSave( $data );
+        //upload image config
+        $config['upload_path']          = $uploadPath;      //upload path
+        $config['allowed_types']        = '*';              //allow types
+        $config['max_size']             = 0;                //file size
+        $config['overwrite']            = TRUE;             //over write file
+
+        $this->load->library('upload', $config);            //load upload library 
+
+        $this->upload->initialize($config);
+
+        if ( ! $this->upload->do_upload('profilePicture')) {      
+
+            echo $this->upload->display_errors();
+            $error = array('error' => $this->upload->display_errors());
+
+        } else {
+
+            $upload_data = $this->upload->data();           //Returns array of containing all of the data related to the file you uploaded.
+            $file_name = $upload_data['file_name'];
+            $uploadPath . '/' . $file_name;                 //upload path with file name
+            $data = array('upload_data' => $this->upload->data());
+
+            $uploadPath . '/' . $file_name;
+
+            $data = array(
+                'fullName' => $this->input->post('fullName', true),
+                'certificateNumber  ' => $this->input->post('certificateNumber', true),
+                'email' => $this->input->post('email', true),
+                'businessAddress' => $this->input->post('businessAddress', true),
+                'telephoneNumber' => $this->input->post('telNumber', true),
+                'userId' => $userId,
+                'password' => $this->input->post('password', true),
+                'websiteUrl' => $this->input->post('website', true),
+                'facebook' => $this->input->post('facebook', true),
+                'country' => $certificateData->country,
+                'city' => $certificateData->city,
+                'location' => $this->input->post('location', true),
+                'imagePath' => $uploadPath
+                ); 
+
+            $this->User->professionalUserSave( $data );
+
+            redirect('home');
+
+        }
       
     }
     
