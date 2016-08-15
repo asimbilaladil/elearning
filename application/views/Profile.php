51782092b842f8701e29839f6966749648e23489
asimbilaladil <asimbilal@Asims-MacBook.local>
2016-08-16 00:53:22 +0500

    add map

--- application/views/Profile.php
+++ application/views/Profile.php
@@ -1,6 +1,9 @@
 <link rel="stylesheet"  href="<?php echo base_url("assets/css/plugin.css") ?>" type="text/css" media="all">
 <link rel="stylesheet"  href="<?php echo base_url("assets/css/frontend.css") ?>" type="text/css" media="all">
 <link rel="stylesheet"  href="<?php echo base_url("assets/css/bootstrap.css") ?>" type="text/css" media="all">
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDtwHugfazWe7vgNlU7kpr9pFw4wdy8ajI&callback=initMap">
+ </script>
+
 <div class="featured-content">
     <div class="substrate">
         <img src="http://www.tdcsinstitute.com/wp-content/themes/academy/images/bgs/site_bg.jpg" class="fullwidth" alt="">          
@@ -15,6 +18,7 @@
 <div class="main-content">
     <div class="row">
         <p style="font-size: 16px;">Use this resource&nbsp;to search for a certified CPE consultant in your area, verify consultant’s certified credentials, check upon consultant’s professional good standing, and&nbsp;schedule&nbsp;an appointment&nbsp;using&nbsp;our central platform that is linked to consultant’s website in “real time”.</p>
+        <div id='gmap_canvas' style='height:200px;width:100%;'></div>
         <p>&nbsp;</p>
         <div class="w2dc-content">
             <div class="w2dc-content" >
@@ -222,3 +226,11 @@
         </div>
     </div>
 </div>
+<style>
+#gmap_canvas img{
+    max-width:none!important;
+    background:none!important
+    }
+</style>
+<script type='text/javascript'>function init_map(){var myOptions = {zoom:8,center:new google.maps.LatLng(24.8614622,67.00993879999999),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(24.8614622,67.00993879999999)});infowindow = new google.maps.InfoWindow({content:'<strong>Mustafa Xaja</strong>'});google.maps.event.addListener(marker, 'click', function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);
+</script>
